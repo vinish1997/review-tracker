a3b87d398653b62ebfba5be840b13126c6104636
Vinish Choudhary <135514876+vinish1997@users.noreply.github.com>
2025-08-28 00:39:10 +0530

    build: add unified build script and placeholder tests

--- review-tracker-ui/src/pages/Reviews.jsx
+++ review-tracker-ui/src/pages/Reviews.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import ReviewTable from "../components/ReviewTable";
-import ReviewForm from "../components/ReviewForm";
+import AddReview from "./AddReview";
+import EditReview from "./EditReview";
 
 export default function Reviews() {
   return (
     <Routes>
       <Route path="/" element={<ReviewTable />} />
-      <Route path="/new" element={<ReviewForm />} />
-      <Route path="/edit/:id" element={<ReviewForm />} />
+      <Route path="/new" element={<AddReview />} />
+      <Route path="/edit/:id" element={<EditReview />} />
     </Routes>
   );
 }

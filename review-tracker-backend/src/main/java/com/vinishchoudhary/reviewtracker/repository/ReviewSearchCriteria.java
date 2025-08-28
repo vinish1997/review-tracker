2e0a276a25b9427103e4ceaab06a2273d4519596
Vinish Choudhary <vinish.choudhary.1997@gmail.com>
2025-08-28 09:15:52 +0530

    test: fix ClassFormatError by removing Lombok from ReviewSearchCriteria and implementing manual builder

--- review-tracker-backend/src/main/java/com/vinishchoudhary/reviewtracker/repository/ReviewSearchCriteria.java
+++ review-tracker-backend/src/main/java/com/vinishchoudhary/reviewtracker/repository/ReviewSearchCriteria.java
@@ -1,18 +1,87 @@
 package com.vinishchoudhary.reviewtracker.repository;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ReviewSearchCriteria {
     private String platformId;
     private String statusId;
     private String mediatorId;
     private String productNameContains;
-}
 
+    public ReviewSearchCriteria() {
+    }
+
+    public ReviewSearchCriteria(String platformId, String statusId, String mediatorId, String productNameContains) {
+        this.platformId = platformId;
+        this.statusId = statusId;
+        this.mediatorId = mediatorId;
+        this.productNameContains = productNameContains;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getPlatformId() {
+        return platformId;
+    }
+
+    public void setPlatformId(String platformId) {
+        this.platformId = platformId;
+    }
+
+    public String getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(String statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getMediatorId() {
+        return mediatorId;
+    }
+
+    public void setMediatorId(String mediatorId) {
+        this.mediatorId = mediatorId;
+    }
+
+    public String getProductNameContains() {
+        return productNameContains;
+    }
+
+    public void setProductNameContains(String productNameContains) {
+        this.productNameContains = productNameContains;
+    }
+
+    public static final class Builder {
+        private String platformId;
+        private String statusId;
+        private String mediatorId;
+        private String productNameContains;
+
+        private Builder() { }
+
+        public Builder platformId(String platformId) {
+            this.platformId = platformId;
+            return this;
+        }
+
+        public Builder statusId(String statusId) {
+            this.statusId = statusId;
+            return this;
+        }
+
+        public Builder mediatorId(String mediatorId) {
+            this.mediatorId = mediatorId;
+            return this;
+        }
+
+        public Builder productNameContains(String productNameContains) {
+            this.productNameContains = productNameContains;
+            return this;
+        }
+
+        public ReviewSearchCriteria build() {
+            return new ReviewSearchCriteria(platformId, statusId, mediatorId, productNameContains);
+        }
+    }
+}
